22dcfa0a517eed692720e5efc0459274fcc20612
István Korompai <korompaii@starschema.net>
2023-01-02 17:18:02 +0100

    merged post preview components

--- components/post/PostPreview.tsx
+++ components/post/PostPreview.tsx
@@ -4,27 +4,29 @@ import Date from 'components/post/Date'
 import type { Post } from 'lib/sanity.queries'
 import Link from 'next/link'
 
-export default function PostPreview(props: Omit<Post, '_id'>) {
-  const { title, coverImage, date, excerpt, authors, slug } = props
+interface Props {
+  post: Omit<Post, '_id'>
+  isFeatured?: boolean
+}
+export default function PostPreview(props: Props) {
+  const { title, coverImage, date, excerpt, slug } = props.post
   return (
     <div>
-      <div className="mb-4">
-        <CoverImage
-          slug={slug}
-          title={title}
-          image={coverImage}
-          priority={false}
-        />
-      </div>
-      <h3 className="mb-3 text-xl font-medium leading-snug">
-        <Link href={`/posts/${slug}`} className="hover:underline">
-          {title}
-        </Link>
-      </h3>
+      <Link href={`/posts/${slug}`} className="hover:underline">
+        <div className="mb-4">
+          <CoverImage
+            slug={slug}
+            title={title}
+            image={coverImage}
+            priority={false}
+          />
+        </div>
+        <h3 className="mb-3 text-xl font-medium leading-snug">{title}</h3>
+      </Link>
       <div className="mb-4 text-lg">
         <Date dateString={date} />
       </div>
-      <p className="mb-4 text-lg leading-relaxed">{excerpt}</p>
+      {/* <p className="mb-4 text-lg leading-relaxed">{excerpt}</p> */}
       {/* {authors.map(a => <Avatar name={a.name} picture={a.picture} key={a.name}/>)} */}
     </div>
   )
