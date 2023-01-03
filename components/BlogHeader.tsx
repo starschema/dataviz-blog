133b66b62156c569b162adc43c73c2efb58e6038
István Korompai <korompaii@starschema.net>
2023-01-03 19:54:01 +0100

    added menu

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -1,13 +1,63 @@
+'use client'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+import HamburgerIcon from './HamburgerIcon'
+import OverlayMenu from './OverlayMenu'
+
+type MenuState = 'closed' | 'closing' | 'open' | 'opening'
 
 export default function BlogHeader(props) {
-  return (
-    <header className="w-min-screen mb-2 flex h-16 flex-row justify-center border-b border-neutral-600 pt-4 pb-2">
-      <Link href="/">
-        <h1 className="text-center text-2xl font-medium tracking-tighter">
-          Blog Name Placeholder
-        </h1>
-      </Link>
+  const router = useRouter()
+  const [menuState, setMenuState] = useState<MenuState>('closed')
+  const menuTransitionDuration = 250 //ms
+
+  const handleMenuClick = () => {
+    if (menuState === 'closed') {
+      setMenuState('opening')
+
+      setTimeout(() => {
+        setMenuState('open')
+      }, menuTransitionDuration)
+    } else if (menuState === 'open') {
+      setMenuState('closing')
+      setTimeout(() => {
+        setMenuState('closed')
+      }, menuTransitionDuration)
+    }
+  }
+  //disable scrolling when the menu is open
+  useEffect(() => {
+    if (menuState === 'open') {
+      document.getElementsByTagName('body')[0].style.overflow = 'hidden'
+      return
+    } else if (menuState === 'closed') {
+      document.getElementsByTagName('body')[0].style.overflow = 'unset'
+      return
+    }
+
+  }, [menuState])
+  const isMenuClosed = menuState === 'closed'
+  const headerHeight = '4rem'
+  const menuHeight = menuState === 'open' ? `calc(100vh - ${headerHeight}` : '0'
+
+  return (<>
+    {/* <header className={`border-b bg-white border-neutral-600 px-4 top-0`} style={{ position: isMenuClosed ? 'relative' : 'sticky' }} > */}
+    <header className={`border-b bg-white border-neutral-600 px-4 top-0`}  >
+      <div className='grid grid-cols-[20px_1fr_20px] ' style={{ height: headerHeight }}>
+
+        <Link href="/" className='col-start-2 col-span-1 flex flex-col justify-center'>
+          <h1 className="text-center text-2xl font-medium">
+            Blog Name Placeholder
+          </h1>
+        </Link>
+        <HamburgerIcon isClosed={true} onClick={() => handleMenuClick()} />
+      </div>
+      {!isMenuClosed && <div className={`min-w-full overflow-hidden transition-[height] `} style={{ height: menuHeight, top: headerHeight, transitionDuration: `${menuTransitionDuration}ms` }}>
+        <OverlayMenu onNavigation={() => handleMenuClick()} />
+      </div>}
     </header>
+  </>
   )
 }
