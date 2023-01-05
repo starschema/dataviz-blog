05f4963d2b0b8280b8e2e651537e2d79e3166170
István Korompai <korompaii@starschema.net>
2023-01-05 14:44:53 +0100

    fix icon in header to be dark mode responsive

--- components/HamburgerIcon.tsx
+++ components/HamburgerIcon.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 export default function HamburgerIcon(props: Props) {
     const { isClosed, className } = props
-    return <button className={className ?? ''} onClick={props.onClick} >
+    return <button className={className ?? ''} onClick={props.onClick}>
         <Image src={isClosed ? HamburgerIconImage : XIconImage} alt='' aria-hidden width={24} height={24} />
     </button >
 }
