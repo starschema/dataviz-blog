c6307c7f77c1ab5ff1f291d5cd9a76a6b803ca6a
István Korompai <korompaii@starschema.net>
2023-01-08 23:10:23 +0100

    added Tableau components first version

---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+import Tableau from "components/blocks/Tableau";
+
+export default function TestPAge() {
+    return <div className='h-80 w-80'>
+        <Tableau alt='this is just a test but alt text is very important' src="https://public.tableau.com/app/profile/julia.borsi/viz/VisitVincent/VisitVincent" />
+    </div>
+}
