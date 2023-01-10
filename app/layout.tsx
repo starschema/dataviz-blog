f22955df199245442fdeace959ab330290add4df
István Korompai <korompaii@starschema.net>
2023-01-10 10:09:24 +0100

    disable dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,8 @@ export default function ContentRootLayout({
 }) {
     return (
         <html lang="en">
-            <body className="bg-white text-black dark:bg-neutral-900 dark:text-white">
+            {/* add dark:bg-neutral-900 dark:text-white to support dark mode*/}
+            <body className="bg-white text-black ">
                 {children}
             </body>
         </html>
