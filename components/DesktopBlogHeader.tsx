336ed626e6a3cc88e23fddbc7e9683226c48d5df
István Korompai <korompaii@starschema.net>
2023-01-12 17:55:50 +0100

    added desktop hero

--- components/DesktopBlogHeader.tsx
+++ components/DesktopBlogHeader.tsx
@@ -4,7 +4,7 @@ export default function DestkopBlogHeader() {
     // this is only ever shown on md screens and up
     const navigationItemClasses = 'px-6 lg:px-8 xl:px-12 outline outline-1 outline-neutral-300 flex items-center pb-1 ml-px';
     return <header className='h-20 xl:h-24 bg-white top-0  outline outline-neutral-300 flex flex-row items-center px-12'>
-        <Link href='/' className='text-xl lg:text-2xl xl:text-4xl flex-grow' >
+        <Link href='/' className='text-xl font-medium lg:text-2xl xl:text-4xl flex-grow' >
             Blog Name Placeholder
         </Link>
         <nav className='h-full'>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,8 @@
 
 export default function Hero() {
   return (
-    <div className="flex aspect-[390/495] flex-col justify-center bg-mobile-hero bg-no-repeat bg-contain">
-      <h2 style={{
-        fontSize: '6vw',
-        paddingLeft: '2.5vw',
-        paddingBottom: '8vw'
-      }}>
+    <div className="flex aspect-[390/495] flex-col justify-center bg-mobile-hero bg-no-repeat bg-contain sm:bg-desktop-hero sm:aspect-[1300/495]">
+      <h2 className='relative text-xl mb-6 pl-2 leading-relaxed xs:text-2xl sm:text-lg md:text-xl lg:text-2xl xl:text-4xl 2xl:text-5xl sm:top-1 sm:left-4 xl:-top-3 xl:left-9 2xl:-top-8 2xl:left-12 '>
         a blog by the dataviz team <br /> at Starschema
       </h2>
     </div >
