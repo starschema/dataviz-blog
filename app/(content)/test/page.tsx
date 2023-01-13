ed3e28ec68c37d279e2264dc4d8bf3eea5a922b4
István Korompai <korompaii@starschema.net>
2023-01-13 15:10:50 +0100

    WIP: work on the Tableau component that goes into the header

--- app/(content)/test/page.tsx
+++ app/(content)/test/page.tsx
@@ -3,7 +3,7 @@ import Tableau from "components/tableau/Tableau";
 import { Suspense } from "react";
 
 export default function TestPAge() {
-    return <div className='h-80 w-80'>
+    return <div className=''>
         <Suspense>
             {/* @ts-expect-error Server Component */}
             <Tableau url='https://public.tableau.com/app/profile/zsofia.nika/viz/Whichcountrygetsthemostpaidvacationdays_16711136524000/Vacation' alt='this is an alt' />
--- components/tableau/Tableau.tsx
+++ components/tableau/Tableau.tsx
@@ -1,20 +1,28 @@
 import { calculatePngDimensions } from 'lib/calculatePngDimensions';
 import { imageUrlFromDashboardUrl } from 'lib/tableauUtils';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import Overlay from './Overlay';
 
 interface Props {
     url: string,
-    alt?: string
+    alt?: string,
+    className?: string,
 }
 
 export default async function Tableau(props: Props) {
     const imageUrl = imageUrlFromDashboardUrl(props.url);
     const dimensions = await calculatePngDimensions(imageUrl);
+    const imageHeight = 300;
     const aspectRatio = dimensions.width / dimensions.height;
-    return <div style={{ height: `300px`, aspectRatio: aspectRatio }} className='relative'>
-        <Image src={imageUrl} alt={props.alt} height={300} width={300 * aspectRatio} />
-        <Overlay url={props.url} alt={props.alt} imageWidth={dimensions.width} imageHeight={dimensions.height} />
+    const containerWidth = imageHeight * aspectRatio;
+    return <div className={props.className}>
+        <Link className='underline text-right block mb-2 underline-offset-2 decoration-blue-600' style={{ width: `${containerWidth}px` }} href={props.url} target='_blank'>Open the viz on Tableau Public</Link>
+        <div style={{ height: `${imageHeight}px`, aspectRatio: aspectRatio }} className='relative outline outline-[#C676BD] outline-[5px] rounded-lg overflow-hidden shadow-[#C676BD] shadow-[10px_13px_0px_0px_rgba(0,0,0,1)]'>
+            <Image src={imageUrl} alt={props.alt} height={300} width={containerWidth} />
+            <Overlay url={props.url} alt={props.alt} imageWidth={dimensions.width} imageHeight={dimensions.height} />
+        </div>
     </div>
+
 }
