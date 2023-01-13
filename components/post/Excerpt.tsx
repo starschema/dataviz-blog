07667a57690b43757ce51585259173aa6d995a4d
István Korompai <korompaii@starschema.net>
2023-01-13 15:12:01 +0100

    WIP: desktop version of the blog header

--- components/post/Excerpt.tsx
+++ components/post/Excerpt.tsx
@@ -1,6 +1,6 @@
 export default function Excerpt(props) {
   return (
-    <div className="mb-8 mt-10 text-xl leading-relaxed font-bold">
+    <div className="mb-8 mt-10 text-xl leading-relaxed font-bold row-start-2 col-span-5">
       <p>{props.excerpt}</p>
     </div>
   )
