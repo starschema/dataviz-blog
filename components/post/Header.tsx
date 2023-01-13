7d2dcd98bef1bcf03487d2ed8bdb7f50412a63fe
István Korompai <korompaii@starschema.net>
2023-01-13 15:12:01 +0100

    WIP: desktop version of the blog header

--- components/post/Header.tsx
+++ components/post/Header.tsx
@@ -1,33 +1,37 @@
+import Tableau from 'components/tableau/Tableau'
 import type { Post } from 'lib/sanity.queries'
+import { Suspense } from 'react'
 
 import CoverImage from './CoverImage'
-import Date from './Date'
 import Excerpt from './Excerpt'
+import HeaderMeta from './HeaderMeta'
 import PostTitle from './Title'
 
 export default function PostHeader(
   props: Pick<
     Post,
-    'title' | 'coverImage' | 'date' | 'authors' | 'slug' | 'excerpt'
+    'title' | 'coverImage' | 'date' | 'authors' | 'slug' | 'excerpt' | 'layoutType'
   >
 ) {
   // FIXME: the width of this header does not match the body at large screens. this might be an issue
-  const { title, coverImage, date, authors, slug } = props
-  const authorNames = authors?.map((author) => author.name).join(', ')
+  const { title, coverImage, date, authors, slug, layoutType } = props
+
+  const visualComponent = layoutType?.layout === 'tableau' ? (
+    <Suspense>
+      {/* @ts-expect-error Server Component */}
+      <Tableau url={layoutType.tableauUrl} alt='missing for now FIXME' className='' />
+    </Suspense>
+  ) : (
+    <CoverImage title={title} image={coverImage} priority slug={slug} />
+  )
   return (
-    <>
+    <div className='md:grid md:grid-cols-8 md:gap-5 mx-auto max-w-4xl md:mb-10 '>
       <PostTitle>{title}</PostTitle>
-      <div className="max-w-2xl text-sm">
-        <p className="mb-4 uppercase">
-          <span className="text-neutral-400">Written by: </span>
-          <span className="font-medium">{authorNames}</span>
-        </p>
+      <HeaderMeta date={date} authors={authors} />
+      <div className='col-span-3 col-start-1 row-span-2 row-start-2 flex justify-end mr-8'>
+        {visualComponent}
       </div>
-      <div className="mb-6 text-sm">
-        <Date dateString={date} />
-      </div>
-      <CoverImage title={title} image={coverImage} priority slug={slug} />
       <Excerpt excerpt={props.excerpt} />
-    </>
+    </div>
   )
 }
--- components/post/Title.tsx
+++ components/post/Title.tsx
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="mb-8 mt-10 text-4xl font-bold leading-tight">
+    <h1 className="mb-8 mt-10 text-4xl font-bold leading-tight col-span-8 md:mb-0">
       {children}
     </h1>
   )
