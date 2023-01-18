18e30949eea06f9817188a0a87cfcf9cfbd43cbc
Istvan Korompai <korompaii@starschema.net>
2023-01-18 18:03:45 +0100

    fix HamburgerIcon after SVGR install

--- components/HamburgerIcon.tsx
+++ components/HamburgerIcon.tsx
@@ -1,17 +1,22 @@
 import Image from 'next/image'
+import HamburgerIconImage from 'public/images/hamburger-icon.svg'
+import XIconImage from 'public/images/x-icon.svg'
 import { MouseEventHandler } from 'react'
 
-import HamburgerIconImage from '../public/images/hamburger-icon.svg'
-import XIconImage from '../public/images/x-icon.svg'
-
 interface Props {
-    isClosed: boolean
-    className?: string
-    onClick: MouseEventHandler<HTMLButtonElement>
+  isClosed: boolean
+  className?: string
+  onClick: MouseEventHandler<HTMLButtonElement>
 }
 export default function HamburgerIcon(props: Props) {
-    const { isClosed, className } = props
-    return <button className={className ?? ''} onClick={props.onClick}>
-        <Image src={isClosed ? HamburgerIconImage : XIconImage} alt='' aria-hidden width={24} height={24} />
-    </button >
+  const { isClosed, className } = props
+  return (
+    <button className={className ?? ''} onClick={props.onClick}>
+      {isClosed ? (
+        <HamburgerIconImage aria-hidden width={24} height={24} />
+      ) : (
+        <XIconImage aria-hidden width={24} height={24} />
+      )}
+    </button>
+  )
 }
