f00035896dd4d1bc5ac035074f9145a70fbc5c82
Istvan Korompai <korompaii@starschema.net>
2023-01-18 11:54:06 +0100

    fix: remove ts-expect-error from Tableau componentn in post header

--- components/post/Header.tsx
+++ components/post/Header.tsx
@@ -10,25 +10,35 @@ import PostTitle from './Title'
 export default function PostHeader(
   props: Pick<
     Post,
-    'title' | 'coverImage' | 'date' | 'authors' | 'slug' | 'excerpt' | 'layoutType'
+    | 'title'
+    | 'coverImage'
+    | 'date'
+    | 'authors'
+    | 'slug'
+    | 'excerpt'
+    | 'layoutType'
   >
 ) {
   // FIXME: the width of this header does not match the body at large screens. this might be an issue
   const { title, coverImage, date, authors, slug, layoutType } = props
 
-  const visualComponent = layoutType?.layout === 'tableau' ? (
-    <Suspense>
-      {/* @ts-expect-error Server Component */}
-      <Tableau url={layoutType.tableauUrl} alt='missing for now FIXME' className='' />
-    </Suspense>
-  ) : (
-    <CoverImage title={title} image={coverImage} priority slug={slug} />
-  )
+  const visualComponent =
+    layoutType?.layout === 'tableau' ? (
+      <Suspense>
+        <Tableau
+          url={layoutType.tableauUrl}
+          alt="missing for now FIXME"
+          className=""
+        />
+      </Suspense>
+    ) : (
+      <CoverImage title={title} image={coverImage} priority slug={slug} />
+    )
   return (
-    <div className='md:grid md:grid-cols-8 md:gap-5 mx-auto max-w-4xl md:mb-10 '>
+    <div className="mx-auto max-w-4xl md:mb-10 md:grid md:grid-cols-8 md:gap-5 ">
       <PostTitle>{title}</PostTitle>
       <HeaderMeta date={date} authors={authors} />
-      <div className='col-span-3 col-start-1 row-span-2 row-start-2 flex justify-end mr-8'>
+      <div className="col-span-3 col-start-1 row-span-2 row-start-2 mr-8 flex justify-end">
         {visualComponent}
       </div>
       <Excerpt excerpt={props.excerpt} />
