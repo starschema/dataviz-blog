6e8064d2f6aed7da4ad5f7fdbd8d83de3f595d24
Istvan Korompai <korompaii@starschema.net>
2023-01-18 18:04:15 +0100

    responsive text position and sizing in Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,9 @@
-
 export default function Hero() {
   return (
-    <div className="flex aspect-[390/495] flex-col justify-center bg-mobile-hero bg-no-repeat bg-contain sm:bg-desktop-hero sm:aspect-[1300/495]">
-      <h2 className='relative text-xl mb-6 pl-2 leading-relaxed xs:text-2xl sm:text-lg md:text-xl lg:text-2xl xl:text-4xl 2xl:text-5xl sm:top-1 sm:left-4 xl:-top-3 xl:left-9 2xl:-top-8 2xl:left-12 '>
+    <div className="relative aspect-[390/495] bg-mobile-hero bg-contain bg-no-repeat sm:aspect-[1300/495] sm:bg-desktop-hero">
+      <h2 className="backdrop: absolute top-[48vw] left-[4vw] text-[5vw] sm:top-[calc(13.33vw+8px)] sm:left-[calc(2vw+8px)] sm:text-[min(2vw,36px)]">
         a blog by the dataviz team <br /> at Starschema
       </h2>
-    </div >
+    </div>
   )
 }
