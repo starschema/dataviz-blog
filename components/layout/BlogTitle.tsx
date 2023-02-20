18c60b5c7957a2bce1b0afd46a5a5459fff0fbc3
Istvan Korompai <korompaii@starschema.net>
2023-02-20 22:08:04 +0100

    fix: tableau expand a11y

--- components/layout/BlogTitle.tsx
+++ components/layout/BlogTitle.tsx
@@ -6,8 +6,8 @@ export default function BlogTitle() {
       href="/"
       className="col-span-1 col-start-2 flex items-center justify-center md:block md:flex-grow"
     >
-      <span className="text-center text-xl font-medium xs:text-2xl md:text-left lg:text-2xl xl:text-4xl">
-        The Viz Collective
+      <span className="text-center text-xl font-light xs:text-2xl md:text-left lg:text-2xl xl:text-4xl">
+        the<strong className="font-medium">viz</strong>collective
       </span>
     </Link>
   )
--- components/shared/Modal.tsx
+++ components/shared/Modal.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { useIsClient, useLockedBody, useOnClickOutside } from 'usehooks-ts'
 
+import XIconImage from '@/public/images/x-icon.svg'
+
 interface Props {
   children: React.ReactNode
   className?: string
@@ -13,22 +15,36 @@ const Modal = (props: Props) => {
   const childrenWrapperRef = useRef<HTMLDivElement>(null)
   const { children, onClose, isOpen } = props
   const isClient = useIsClient()
-  useOnClickOutside(childrenWrapperRef, () => {
-    props.onClose()
-  })
 
   useLockedBody(isOpen, 'root')
+
+  const wrapperClickHandler = (event) => {
+    if (event.target == event.currentTarget) {
+      onClose()
+    }
+  }
 
   if (!isClient) return null
 
   const modalRoot = document.getElementById('modal-root')
   const modal = (
-    <div className="fixed top-0 left-0 z-50 flex h-screen w-screen  justify-center overflow-auto bg-neutral-700/50 backdrop-blur-sm backdrop-filter ">
-      {/* FIXME: add an exit button here */}
-      <div className="absolute m-10" ref={childrenWrapperRef}>
+    <dialog
+      className="fixed top-0 left-0 z-50 flex h-screen w-screen  cursor-pointer justify-center overflow-auto bg-neutral-700/50 backdrop-blur-sm backdrop-filter "
+      onClick={wrapperClickHandler}
+    >
+      <button
+        className="absolute top-4 right-4"
+        aria-label="Close the Image Preview"
+        type="button"
+        onClick={onClose}
+        autoFocus
+      >
+        <XIconImage aria-hidden className="stroke-black hover:stroke-2" />
+      </button>
+      <div className="absolute m-10 cursor-auto" ref={childrenWrapperRef}>
         {children}
       </div>
-    </div>
+    </dialog>
   )
 
   return props.isOpen ? createPortal(modal, modalRoot) : null
