fe01c8b493b3aaaa0aff626458ae5b7122f1a0c3
István Korompai <korompaii@starschema.net>
2023-03-03 20:04:08 +0100

    feat: implement the design of the Author Card (#48)

--- components/post/AuthorBioBox.tsx
+++ components/post/AuthorBioBox.tsx
@@ -6,19 +6,22 @@ interface Props {
 }
 export default function AuthorBioBox(props: Props) {
   return (
-    <div className="mx-auto mt-12 mb-8 max-w-4xl bg-blue-100 px-6 pt-6 pb-0">
+    <div className="mx-auto mt-12 mb-8 max-w-4xl bg-blue-50 px-6 pt-6 pb-0">
       <div className="text-sm font-light text-neutral-400">
         <p>about the {props.authors.length > 1 ? 'authors' : 'author'}</p>
       </div>
-      {props.authors.map((author, idx) => {
-        return (
-          <AuthorBio
-            name={author.name}
-            bio={author.bio}
-            key={`${author.name}-${idx}`}
-          />
-        )
-      })}
+      <ul className="flex flex-col gap-2 py-2">
+        {props.authors.map((author, idx) => {
+          return (
+            <AuthorBio
+              name={author.name}
+              bio={author.bio}
+              key={`${author.name}-${idx}`}
+              isFixedOpen={true}
+            />
+          )
+        })}
+      </ul>
     </div>
   )
 }
