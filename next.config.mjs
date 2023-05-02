d682e43f5aa95e96e69870d841844b5be716f9e4
Istvan Korompai <korompaii@starschema.net>
2023-05-02 13:18:58 +0100

    fix: add standalone to nwxt config

--- next.config.mjs
+++ next.config.mjs
@@ -51,6 +51,7 @@ const securityHeaders = [
 ]
 
 const config = withPlausibleProxy()({
+  output: "standalone",
   images: {
     remotePatterns: imageSrcDomains.map((domain) => ({ hostname: domain })),
   },
